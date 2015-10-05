103d52c657faa4a06fe2943016b6e5ea088bdf55
HeyStenson <h.n.stenson@gmail.com>
2015-10-05 14:26:58 -0700

    fixed conflict

--- project2/app/controllers/application_controller.rb
+++ project2/app/controllers/application_controller.rb
@@ -3,10 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-<<<<<<< HEAD
   include SessionsHelper
-=======
   include PerformerSessionsHelper
-  
->>>>>>> 4e37c47854226b0cbcdb49e814c363d44d36388e
+
 end
