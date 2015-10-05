98354eb3ed657e885851a2d1e5c5775122596a37
Lisa Lehrman <lisa.lehrman@gmail.com>
2015-10-05 11:02:54 -0700

    pushing before mass reboot

--- project2/app/controllers/clients_controller.rb
+++ project2/app/controllers/clients_controller.rb
@@ -5,25 +5,24 @@ def index
     render :index
   end
 
-def show
-  id = params[:id]
-  @client = Client.find(params[:id])
-
-  render :show
-  end
-
-  def new
+def new
     @client = Client.new
     render :new
   end
 
-  def create
+def create
     client_params = params.require(:client).permit(:firstname, :lastname, :email, :password)
     
     @client = Client.create(client_params)
     redirect_to("/clients/#{@client.id}" )
   end
 
+def show
+  id = params[:id]
+  @client = Client.find(params[:id])
+
+  render :show
+  end  
 
 
   def edit #has views
@@ -34,20 +33,18 @@ def show
   end
 
   def update
-    @client = Client.find(params[:id])
-  if @client.update(client_params)
-    redirect_to("/clients/#{@client.id}" )
-  else 
-    render :edit
-    end
+   client_id = params[:id]
+      client = Client.find(client_id)
+      # get updated data
+      updated_attributes = params.require(:client).permit(:firstname, :lastname)
+      # update the user
+      client.update_attributes(updated_attributes)
+      #redirect to show
+      redirect_to "/client/#{client.id}"  # <-- go to show
   end
 
-  def client_params
-      params.require(:client).permit(:firstname, :lastname)
-    end
-end
   
-
 
   def destroy
   end
+end
