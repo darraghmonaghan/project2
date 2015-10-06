18776b739638a2455691df851040ee579d1314a1
HeyStenson <h.n.stenson@gmail.com>
2015-10-05 19:16:17 -0700

    carousels for performer profile

--- project2/app/models/performer.rb
+++ project2/app/models/performer.rb
@@ -15,13 +15,13 @@ class Performer < ActiveRecord::Base
 	has_attached_file :avatar, styles: { medium: "100x100>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
-  	has_attached_file :image1, styles: { medium: "100x100>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  	has_attached_file :image1, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :image1, content_type: /\Aimage\/.*\Z/
 
-  	has_attached_file :image2, styles: { medium: "100x100>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  	has_attached_file :image2, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :image2, content_type: /\Aimage\/.*\Z/
 
-  	has_attached_file :image3, styles: { medium: "100x100>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  	has_attached_file :image3, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :image3, content_type: /\Aimage\/.*\Z/
 
 	def self.confirm(params)
